257e95fd8f8c76a5239cd7cd90b8ae989acc4de6
Kanhakaushik <kaushikkanha6396@gmail.com>
2024-02-11 17:38:53 +0530

    loginAndregistrestion

--- HMS/app/models.py
+++ HMS/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DataSignup(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    password=models.CharField(max_length=200)
+    cpassword=models.CharField(max_length=200)
+    
+    
